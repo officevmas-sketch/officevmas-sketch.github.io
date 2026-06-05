45ed844ca0404b623f0b34605bb1e42763868408
officevmas-sketch <office.vmas@gmail.com>
2026-06-06 00:35:36 +0530

    Add files via upload

---
+++ firebase-config.js
@@ -0,0 +1,14 @@
+
+window.firebaseConfig = {
+  apiKey: "AIzaSyBbh9bAUg21T3i94pVD50Ak8diV0qFvRCE",
+  authDomain: "vmas-intercom.firebaseapp.com",
+  databaseURL: "https://vmas-intercom-default-rtdb.firebaseio.com",
+  projectId: "vmas-intercom",
+  storageBucket: "vmas-intercom.firebasestorage.app",
+  messagingSenderId: "430555520466",
+  appId: "1:430555520466:web:39873293db290696bfbc0b",
+  measurementId: "G-MTTGDZ6B8F"
+};
+
+firebase.initializeApp(window.firebaseConfig);
+const db = firebase.database();
