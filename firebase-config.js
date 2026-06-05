c522e656cbfe74b0f218f0135fe932fc47fe543e
officevmas-sketch <office.vmas@gmail.com>
2026-06-05 22:17:14 +0530

    Update firebase-config.js

--- firebase-config.js
+++ firebase-config.js
@@ -1,12 +1,13 @@
 
 window.firebaseConfig = {
-  apiKey: "PASTE_API_KEY",
-  authDomain: "YOUR_PROJECT.firebaseapp.com",
-  databaseURL: "https://YOUR_PROJECT-default-rtdb.firebaseio.com",
-  projectId: "YOUR_PROJECT",
-  storageBucket: "YOUR_PROJECT.appspot.com",
-  messagingSenderId: "123456789",
-  appId: "APP_ID"
+  apiKey: "AIzaSyBbh9bAUg21T3i94pVD50Ak8diV0qFvRCE",
+  authDomain: "vmas-intercom.firebaseapp.com",
+  databaseURL: "https://vmas-intercom-default-rtdb.firebaseio.com",
+  projectId: "vmas-intercom",
+  storageBucket: "vmas-intercom.firebasestorage.app",
+  messagingSenderId: "430555520466",
+  appId: "1:430555520466:web:39873293db290696bfbc0b",
+  measurementId: "G-MTTGDZ6B8F"
 };
 
 firebase.initializeApp(window.firebaseConfig);
